4cee903d849b6f741ae0312f151d017e91b9efd3
mouradaitelouhab <mouradprotech@gmail.com>
2025-07-18 15:31:00 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5174,
+    allowedHosts: ["localhost", "5174-i81uy21aw1rsa93czxw00-bbbd7599.manusvm.computer"],
+  }
+})
